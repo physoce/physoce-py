0ab41e385e67ef51c0eefc32e9e2d2c10b9087d6
Tom Connolly <tconnolly@mlml.calstate.edu>
2015-09-11 18:13:46 -0700

    moved lobo module to physoce package

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='physoce',
+      version='0.1',
+      description='Python tools for Physical Oceanography at Moss Landing Marine Labs',
+      author='Tom Connolly',
+      author_email='tconnolly@mlml.calstate.edu',
+      packages=['physoce'])
