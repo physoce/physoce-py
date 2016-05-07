25734cedff8488c8b5b63e49d098b71a12b9861c
Tom Connolly <tconnolly@mlml.calstate.edu>
2016-05-06 18:32:47 -0700

    added PL66 filter in tseries

--- physoce/tseries.py
+++ physoce/tseries.py
@@ -3,7 +3,7 @@ from scipy import stats, signal
 
 def hanning(x,N):
     """ 
-Filter a time series x with a Hanning window of length N
+Filter a time series x with a Hanning window of length N.
     
 Inputs:
 x - a numpy array to be filtered
@@ -16,16 +16,16 @@ Output: numpy array of filtered time series
     xf = _filt(x,wts)
     return xf
     
-def lancz(x,cutoff=40,dt=1):
+def lancz(x,dt=1,T=40):
     """ 
-Filter a time series x with cosine-Lanczos filter
+Filter a time series x with cosine-Lanczos filter.
 
-The default cutoff (half power period) of 40 hours corresponds to a frequency of 0.6 cpd. A cutoff of 34.29h corresponds to 0.7 cpd. The 40 hour cutoff is more effective at reducing diurnal-band variability but shifts periods of variability in low passed time series to >2 days.
+The default half amplitude period of 40 hours corresponds to a frequency of 0.6 cpd. A half amplitude period of 34.29h corresponds to 0.7 cpd. The 40 hour half amplitude period is more effective at reducing diurnal-band variability but shifts periods of variability in low passed time series to >2 days.
     
 Inputs:
 x - a numpy array to be filtered
-cutoff - half-power period (hours), default = 40
 dt - sample interval (hours), default = 1
+T - half-amplitude period (hours), default = 40
     
 Output: numpy array of filtered time series, same size as input with ends NaN values at start and end.
 
@@ -33,14 +33,45 @@ Reference: Emery and Thomson, 2004, Data Analysis Methods in Physical Oceanograp
     """
 
     cph = 1./dt   # samples per hour
-    nwts = int(120*cph) # number of weights
+    nwts = int(round(120*cph)) # number of weights
     
-    # create the filter weights
+    # create filter weights
     wts = signal.firwin(nwts, 
-                        1./cutoff, 
+                        1./T, 
                         window='hanning', 
                         nyq=cph/2.)  
                         
+    xf = _filt(x,wts)
+    return xf
+    
+def pl66(x,dt=1,T=33):
+    """
+Filter a time series x with the PL66 filter.
+    
+Inputs:
+x - a numpy array to be filtered
+dt - sample interval (hours), default = 1
+T - half-amplitude period (hours), default = 33
+    
+Output: numpy array of filtered time series, same size as input with ends NaN values at start and end.    
+    
+Reference: Rosenfeld (1983), WHOI Technical Report 85-35
+Matlab code: http://woodshole.er.usgs.gov/operations/sea-mat/bobstuff-html/pl66tn.html
+    """
+    
+    Tn=float(T)/dt # normalized cutoff period
+    fqn=1./Tn # normalized cutoff frequency
+    nw = int(round(2.*T/dt)) # number of weights on one side
+    
+    # create filter weights
+    j = np.arange(1,nw)
+    tn = np.pi*j
+    den=fqn*fqn*tn**3
+    wts = (2*np.sin(2*fqn*tn)-np.sin(fqn*tn)-np.sin(3*fqn*tn))/den 
+    
+    # make symmetric
+    wts = np.hstack((wts[::-1],2*fqn,wts))
+    
     xf = _filt(x,wts)
     return xf
     
@@ -54,12 +85,13 @@ x - the time series
 wts - the filter weights
 
 Output: the filtered time series
-
     """
+
+    ### TO DO: option to detrend data before filtering, then add trend back in after, remove nans and use np.linalg.lstsq ###    
     
     wtsn = wts/sum(wts) # normalize weights so sum = 1
-    xf = signal.convolve(x,wtsn,mode='same')
-
+    xf = signal.convolve(x,wtsn,mode='same')  
+    
     # pad ends of time series
     nwts = len(wts) # number of filter weights
     npad = np.ceil(0.5*nwts) 
