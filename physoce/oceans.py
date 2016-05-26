0c9c65f4de16c686cd1a1779ef9c915e7dd83100
Tom Connolly <tconnolly@mlml.calstate.edu>
2016-05-26 15:41:16 -0700

    added test for ustokes function, modified variable names and orientation of default depth array, but output values still the same

--- physoce/oceans.py
+++ physoce/oceans.py
@@ -6,23 +6,26 @@ Tom Connolly 2014
 import numpy as np
 
 def wavedisp(wavper,h):
-	""" (omega,k,Cph,Cg) = wavedisp(wavper,h)
-	------------------
-	Returns [omega,k,Cph,Cg]
+	""" 
+(omega,k,Cph,Cg) = wavedisp(wavper,h)
+------------------
+Returns [omega,k,Cph,Cg]
 	
-	Inputs (can use arrays): 	
-				wavper	- wave period
-				h	- water depth
-	Outputs: 	
-				omega	- wave frequency
-				k	- wave number	
-				Cph	- phase speed
-				Cg	- group velocity 
+Inputs (can use arrays): 	
+			wavper	- wave period [s]
+			h	- water depth [m]
+   
+Outputs: 	
+			omega	- angular wave frequency [radians/s]
+			k	- angular wave number	 [radians/m]
+			Cph	- phase speed [m/s]
+			Cg	- group velocity [m/s]
     """
 			
 	""" T Connolly 2014
 	based on Matlab function wavedisp.m from S Lentz """
 	
+    # make sure inputs are arrays
 	wavper=np.array(wavper)
 	h=np.array(h)
 	
@@ -32,8 +35,8 @@ def wavedisp(wavper,h):
 	
 	x = np.sqrt(c)	
 	
-	d = 100*np.ones(np.shape(wavper))
-	tol = 5e-16
+   	d = 100*np.ones(np.shape(wavper))
+	tol = 5.*np.finfo(float).eps
 	while any(d>tol):
 		f1=x*np.tanh(x)-c
 		f2=x*(1/np.cosh(x))**2+np.tanh(x)
@@ -45,39 +48,42 @@ def wavedisp(wavper,h):
 	
 	return (omega,k,Cph,Cg)
 
-def ustokes(Hsig,wavper,wdepth,zst=()):
-	""" (ust,zst) = ustokes(Hsig,wavper,wdepth,zst=()):
-	Stokes drift velocity
-	---------------------
-	Inputs: 	Hsig	- Significant wave height [m]
-				wavper	- wave period [s]
-				wdepth	- bottom depth [m]
-				zst	- depths of calculation [m] 
-					(optional, every 1m if not given)
-	Outputs:	ust	- Stokes drift [m/s]
-				zst	- depths of calculation
+def ustokes(Hsig,wavper,h,zst=()):
+	""" 
+ust,zst = ustokes(Hsig,wavper,h,zst=()):
+Stokes drift magnitude
+---------------------
+Inputs:     Hsig	- significant wave height [m]
+            wavper	- wave period [s]
+            h     - bottom depth [m]
+            zst	- depths of calculation [m] 
+            (optional, every 1m if not given)
+            
+Outputs:	ust	- Stokes drift [m/s]
+			zst	- depths of calculation [m]
+    """
 	
-	T Connolly 2014
+	"""T Connolly 2014
 	based on Matlab function ustokes.m from S Lentz"""
 	
 	if not any(zst):
-		zst = np.arange(-wdepth,0)
+		zst = np.arange(0,-h-1,-1)
 		
-	(omega,wavnum,Cph,Cg)=wavedisp(wavper,wdepth)
-	A=Hsig**2*omega*wavnum/(16*np.sinh(wavnum*wdepth)**2)
-	ust=A*np.cosh(2*wavnum*(zst+wdepth))
+	(omega,k,Cph,Cg)=wavedisp(wavper,h)
+	A=Hsig**2*omega*k/(16*np.sinh(k*h)**2)
+	ust=A*np.cosh(2*k*(zst+h))
 		
 	return (ust,zst)
  
 if __name__ == '__main__':
-    omega,k,Cph,Cg = wavedisp([7,10],12)
-
+    
+    ### Test wavedisp ###
+    # values from original Matlab function
     mat_omega = np.array([0.8976,0.6283])
     mat_k = np.array([0.0990,0.0630])
     mat_Cph = np.array([9.0631,9.9667])
     mat_Cg = np.array([6.5488,8.4740])
-    
-
+    omega,k,Cph,Cg = wavedisp([7,10],12)
     test = np.isclose(np.array([omega,k,Cph,Cg]),
                       np.array([mat_omega,mat_k,mat_Cph,mat_Cg]),
                           atol = 1e-4)             
@@ -85,3 +91,14 @@ if __name__ == '__main__':
         print('wavedisp test: passed')
     else:
         raise ValueError('wavedisp test: failed')
+        
+    ### Test ustokes ###
+    # values from original Matlab function
+    ust0_mat = 0.0545
+    ust,zst = ustokes(2,7,12)
+    ust0 = ust[0]
+    test = np.isclose(ust0_mat,ust0,atol=1e-4)
+    if test:
+        print('ustokes test: passed')
+    else:
+        raise ValueError('ustokes test: failed')        
