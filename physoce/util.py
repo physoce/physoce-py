d8152c8a7d5a531ba679f57fbaac2a48af43d944
Tom Connolly <tconnolly@mlml.calstate.edu>
2017-03-28 17:52:50 -0700

    added angle conversion functions

--- physoce/util.py
+++ physoce/util.py
@@ -3,6 +3,7 @@
 General purpose functions.
 """
 from datetime import datetime
+import numpy as np
 
 def list2date(datestr_list,fmt='%a %b %d %H:%M:%S %Y'):
     '''Convert a list of date strings to datetime format.
@@ -18,3 +19,39 @@ def list2date(datestr_list,fmt='%a %b %d %H:%M:%S %Y'):
     '''
     datetime_list = [datetime.strptime(datestr, fmt) for datestr in datestr_list]
     return datetime_list
+    
+def compass2polar(theta):
+    '''
+    Convert an angle from compass direction (clockwise from true North) to direction in polar coordinates (counter-clockwise from x-axis, pointing East).
+    
+    INPUT: 
+    - theta - compass direction (degrees), numpy array
+        
+    OUTPUT:
+    - direction in polar coordinates (degrees), numpy array of same size
+    '''
+    
+    theta = theta*np.pi/180 # convert to radians
+    x = -np.sin(-theta)
+    y = np.cos(-theta)
+    theta_pol = np.arctan2(y,x)
+    theta_pol = theta_pol*180/np.pi # convert back to degrees
+    return theta_pol
+    
+def polar2compass(theta):
+    '''
+    Convert an angle from direction in polar coordinates (counter-clockwise from x-axis, pointing East) to compass direction (clockwise from true North).
+    
+    INPUT: 
+    - theta - compass direction (degrees), numpy array
+        
+    OUTPUT:
+    - direction in polar coordinates (degrees), numpy array of same size
+    '''
+    
+    theta = theta*np.pi/180 # convert to radians
+    x = np.cos(theta)
+    y = np.sin(theta)
+    theta_comp = -np.arctan2(x,-y)
+    theta_comp = theta_comp*180/np.pi # convert back to degrees
+    return theta_comp
