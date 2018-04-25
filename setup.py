c8c8e006dc72f333e75393c7bd72677289071741
Tom Connolly <tconnolly@mlml.calstate.edu>
2018-04-25 11:12:07 -0700

    added sphinx generated docs to gh-pages branch

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='physoce',
-      version='0.1',
-      description='Python tools for Physical Oceanography',
-      author='Tom Connolly',
-      author_email='tconnolly@mlml.calstate.edu',
-      packages=['physoce'])
