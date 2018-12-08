bc5515f1e96975020b83a5c22caaf05e5517f436
Tom Connolly <tconnolly@mlml.calstate.edu>
2018-12-08 11:31:08 -0800

    pypi friendly readme

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup
 
+# read the contents of README file
+# see https://packaging.python.org/guides/making-a-pypi-friendly-readme/
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='physoce',
       version='0.1',
       description='Python tools for Physical Oceanography',
+      long_description=long_description,
+      long_description_content_type='text/markdown'
       author='Tom Connolly',
       author_email='tconnolly@mlml.calstate.edu',
       packages=['physoce'])
